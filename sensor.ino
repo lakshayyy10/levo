f1d8ef53fb487c6c4ee071f6dd45f4488483f1ba
Lakshay Chhabra <lakshay@Delete.lakshayyy15>
2025-01-17 14:56:33 +0530

    sensors code for arduino

---
+++ sensor.ino
@@ -0,0 +1,31 @@
+#include <DHT.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+const int pins[4] = {A0, A1, A2, 3};
+#define Voc_PIN 0
+#define Mics_PIN 1
+#define Capacitive_PIN 2
+#define DHT_PIN 3
+
+DHT dht(pins[DHT_PIN], DHT11);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  float dhtTemp = dht.readTemperature(); 
+  int vocValue = analogRead(pins[Voc_PIN]);
+  int gasValue = analogRead(pins[Mics_PIN]);
+  int moistureValue = analogRead(pins[Capacitive_PIN]);
+  Serial.print(vocValue);
+  Serial.print(",");
+  Serial.print(gasValue);
+  Serial.print(",");
+  Serial.print(moistureValue);
+  Serial.print(",");
+  Serial.println(dhtTemp);
+
+  delay(1000);
+}
